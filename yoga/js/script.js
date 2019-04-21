f695080eaa7a9e39a885aeb287545bc00f367ac8
Pavel Linitskii <linitskii.p@gmail.com>
2019-04-21 09:28:52 +0300

    Мелкие исправления таймера

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -37,12 +37,14 @@ let tab = document.querySelectorAll('.info-header-tab'),
     });
 
     // Таймер
-    
+    // Задаём конечную дату
     let deadline = '2019-04-22';
 
+    // Сколько осталось времени
     function getTimeRemaining(endtime) {
-        
+        // Получаем разницу. Переводим конечную дату в миллисекунды и отнимаем текущую дату
         let t = Date.parse(endtime) - Date.now(),
+        // Высчитываем секунды, минуты, часы и дни
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
             hours = Math.floor((t/1000/60/60) % 24),
@@ -57,27 +59,39 @@ let tab = document.querySelectorAll('.info-header-tab'),
             };
     }
 
+    // Форматируем и устанавливаем значения в разметку
     function setClock(id, endtime) {
+        // Получаем блок таймера по его id
         let timer = document.getElementById(id),
+        // И соотвественно, все элементы блока
             days = timer.querySelector('.days'),
             dayname = timer.querySelector('.dayname'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
+            // Обновляем таймер каждую секунду
             timeInterval = setInterval(updateClock, 1000);
-        
+
+            // Функция для обновления таймера
             function updateClock() {
+                // Помещаем в t результат функции getTimeRemaining (объект)
                 let t = getTimeRemaining(endtime);
-                if (t.days > 0) {
-                    days.textContent = t.days;
-                } else {
+                // Если дней осталось 0 - не выводим
+                if (t.days <= 0) {
                     days.textContent = "";
+                    dayname.textContent = "";
+                } else {
+                    days.textContent = t.days;
+                // Вставляем слово "день" при этом форматируя (дней, дня)
+                    dayname.textContent = formatDays(t.days);                    
                 }
-                dayname.textContent = formatDays(t.days);
+                // Вставляем оставшееся время
                 hours.textContent = formatTime(t.hours);
                 minutes.textContent = formatTime(t.minutes);
                 seconds.textContent = formatTime(t.seconds);
 
+                // Если Дедлайн прошёл - вставляем в таймер 00:00:00,
+                // и останавливаем отсчёт (clearInterval)
                 if (t.total <= 0) {
                     clearInterval(timeInterval);
                     days.textContent = "";
@@ -87,6 +101,8 @@ let tab = document.querySelectorAll('.info-header-tab'),
                 }
             }
 
+            // Функция добавляет 0 к единцам, получаем
+            // 03:04:05 вместо 3:4:5
             function formatTime(time){
                 if ( time < 10) {
                     time = '0' + time;
@@ -94,11 +110,12 @@ let tab = document.querySelectorAll('.info-header-tab'),
                 return time;
             }
 
+            // Форматируем слово день в зависимости от числа
+            // 1 день, 2 дня, 5 дней.
             function formatDays(day) {
                 let sb = '',
                     dayNew = day % 100,
-                    lastFigure = dayNew % 10;
-
+                    lastFigure = day % 10;
                 if (dayNew > 10 && dayNew < 20) {
                   sb = 'ей';
                 }
@@ -108,9 +125,6 @@ let tab = document.querySelectorAll('.info-header-tab'),
                 else if (lastFigure > 1 && lastFigure < 5) {
                   sb = 'дня';
                 }
-                else if (lastFigure == 0 ) {
-                    sb = '';
-                  }
                 else {
                   sb = 'дней';
                 }
