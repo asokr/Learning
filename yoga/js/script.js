486e85b840ce1cdafc8e3f94440aeaaa953e249c
Pavel Linitskii <linitskii.p@gmail.com>
2019-04-21 08:57:30 +0300

    Добавлен таймер отсчета день:час:минуты:секунды

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,7 +1,8 @@
+
 window.addEventListener('DOMContentLoaded', function() {
 
 'use strict';
-
+// Табы
 let tab = document.querySelectorAll('.info-header-tab'),
     info = document.querySelector('.info-header'),
     tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,5 +35,89 @@ let tab = document.querySelectorAll('.info-header-tab'),
             }
         }
     });
+
+    // Таймер
+    
+    let deadline = '2019-04-22';
+
+    function getTimeRemaining(endtime) {
+        
+        let t = Date.parse(endtime) - Date.now(),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/1000/60/60) % 24),
+            days = Math.floor((t/1000/60/60/24));
+
+            return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours,
+            'days': days
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            dayname = timer.querySelector('.dayname'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        
+            function updateClock() {
+                let t = getTimeRemaining(endtime);
+                if (t.days > 0) {
+                    days.textContent = t.days;
+                } else {
+                    days.textContent = "";
+                }
+                dayname.textContent = formatDays(t.days);
+                hours.textContent = formatTime(t.hours);
+                minutes.textContent = formatTime(t.minutes);
+                seconds.textContent = formatTime(t.seconds);
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                    days.textContent = "";
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+                }
+            }
+
+            function formatTime(time){
+                if ( time < 10) {
+                    time = '0' + time;
+                }
+                return time;
+            }
+
+            function formatDays(day) {
+                let sb = '',
+                    dayNew = day % 100,
+                    lastFigure = dayNew % 10;
+
+                if (dayNew > 10 && dayNew < 20) {
+                  sb = 'ей';
+                }
+                else if (lastFigure == 1) {
+                  sb = "день";
+                }
+                else if (lastFigure > 1 && lastFigure < 5) {
+                  sb = 'дня';
+                }
+                else if (lastFigure == 0 ) {
+                    sb = '';
+                  }
+                else {
+                  sb = 'дней';
+                }
+                return sb;
+            }
+    }
+
+    setClock('timer', deadline);
 
 });
