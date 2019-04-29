6527e54caff3257adef64ca1be5cc2d3f6455921
Pavel Linitskii <linitskii.p@gmail.com>
2019-04-29 08:49:37 +0300

    Добавлен слайдер и калькулятор

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -133,7 +133,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     // Модальное окно
-
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close'),
@@ -189,11 +188,11 @@ window.addEventListener('DOMContentLoaded', () => {
                     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencded');
                     request.onreadystatechange(() => {
                         if (request.readyState < 4) {
-                            resolve()
+                            resolve();
                         } else if (request.readyState === 4 && request.status == 200) {
-                            resolve()
+                            resolve();
                         } else {
-                            reject()
+                            reject();
                         }
                     });
                     request.send(data);
@@ -213,11 +212,100 @@ window.addEventListener('DOMContentLoaded', () => {
                 .then(clearInput);
         });
     }
-
     let form = document.querySelector('.main-form'),
         contactForm = document.querySelector('.contact-form');
-
     sendForm(form);
     sendForm(contactForm);
 
+    // Слайдер
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+        showSlides(slideIndex);
+
+        function showSlides(n) {
+
+            if( n > slides.length) {
+                slideIndex = 1;
+            } else if ( n < 1) {
+                slideIndex = slides.length;
+            }
+
+            slides.forEach((item)=> item.style.display = 'none');
+            dots.forEach((item)=> item.classList.remove('dot-active'));
+
+            slides[slideIndex-1].style.display = 'block';
+            dots[slideIndex-1].classList.add('dot-active');
+        }
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        prev.addEventListener('click', ()=>{
+            plusSlides(-1); 
+        });
+
+        next.addEventListener('click', ()=>{
+            plusSlides(1); 
+        });
+
+        dotsWrap.addEventListener('click', (event)=>{
+            for(let i=0; i < dots.length + 1; i++) {
+                if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                    currentSlide(i);
+                }
+            }
+        });
+
+        //Калькулятор
+        let persons = document.querySelectorAll('.counter-block-input')[0],
+            restDays = document.querySelectorAll('.counter-block-input')[1],
+            place = document.getElementById('select'),
+            totalValue = document.getElementById('total'),
+            personsSum = 0,
+            daysSum = 0,
+            total = 0;
+
+            totalValue.innerHTML = 0;
+
+            persons.addEventListener('change', function() {
+                personsSum = +this.value;
+                total = (daysSum + personsSum)*4000;
+
+                if (restDays.value == '' || persons.value.length === 0) {
+                    totalValue.innerHTML = 0;
+                } else {
+                    totalValue.innerHTML = total;
+                }
+            });
+            
+            restDays.addEventListener('change', function() {
+                daysSum = +this.value;
+                total = (daysSum + personsSum)*4000;
+
+                if (persons.value == '' || restDays.value.length === 0) {
+                    totalValue.innerHTML = 0;
+                } else {
+                    console.log(persons.value.length)
+                    totalValue.innerHTML = total;
+                }
+            });
+
+            place.addEventListener('change', function() {
+                if (restDays.value == '' || persons.value == '') {
+                    totalValue.innerHTML = 0;
+                } else {
+                    let a = total;
+                    totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+                }
+            });
 });
